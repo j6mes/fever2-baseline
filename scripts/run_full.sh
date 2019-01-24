d173f0aba8328194991911c54cea930e6f6a9e06
James Thorne <james@jthorne.co.uk>
2019-01-24 09:41:43 +0000

    lazy run scripts

--- scripts/run_full.sh
+++ scripts/run_full.sh
@@ -3,6 +3,9 @@ export MODEL=fever-da
 mkdir -pv data/model-specific/$MODEL/full/$1
 mkdir -pv data/predictions/$MODEL/full/$1
 
+if [ "$(wc -l < data/$1/$2)" -eq "$(wc -l < data/model-specific/$MODEL/full/$1/$2)" ];
+then echo 'Skipping sampling evidence as this exists';
+else
 python -m fever.evidence.retrieve \
     --index data/index/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz \
     --database data/fever/fever.db \
@@ -10,12 +13,17 @@ python -m fever.evidence.retrieve \
     --out-file data/model-specific/$MODEL/full/$1/$2 \
     --max-page 5 \
     --max-sent 5
+fi
 
-python -m allennlp.run predict \
-    model.tar.gz \
-    data/model-specific/$MODEL/full/$1/$2 \
-    --output-file data/predictions/$MODEL/full/$1/$2 \
-    --predictor fever \
-    --include-package fever.reader \
-    --cuda-device -1 \
-    --silent
+if [ "$(wc -l < data/model-specific/$MODEL/full/$1/$2)" -eq "$(wc -l < data/predictions/$MODEL/full/$1/$2)" ];
+then echo 'Skipping making predictions as this exists';
+else
+    python -m allennlp.run predict \
+        model.tar.gz \
+        data/model-specific/$MODEL/full/$1/$2 \
+        --output-file data/predictions/$MODEL/full/$1/$2 \
+        --predictor fever \
+        --include-package fever.reader \
+        --cuda-device -1 \
+        --silent
+fi
--- scripts/run_oracle.sh
+++ scripts/run_oracle.sh
@@ -3,16 +3,25 @@ export MODEL=fever-da
 mkdir -pv data/model-specific/$MODEL/oracle/$1
 mkdir -pv data/predictions/$MODEL/oracle/$1
 
-python -m fever.evidence.negative_sample_nearest \
-    --index data/index/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz \
-    --in-file data/$1/$2 \
-    --out-file data/model-specific/$MODEL/oracle/$1/$2
+if [ "$(wc -l < data/$1/$2)" -eq "$(wc -l < data/model-specific/$MODEL/oracle/$1/$2)" ];
+then echo 'Skipping sampling evidence as this exists';
+else
+    python -m fever.evidence.negative_sample_nearest \
+        --index data/index/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz \
+        --in-file data/$1/$2 \
+        --out-file data/model-specific/$MODEL/oracle/$1/$2
+fi
 
-python -m allennlp.run predict \
-    model.tar.gz \
-    data/model-specific/$MODEL/oracle/$1/$2 \
-    --output-file data/predictions/$MODEL/oracle/$1/$2 \
-    --predictor fever-oracle \
-    --include-package fever.reader \
-    --cuda-device -1 \
-    --silent
+
+if [ "$(wc -l < data/model-specific/$MODEL/oracle/$1/$2)" -eq "$(wc -l < data/predictions/$MODEL/oracle/$1/$2)" ];
+then echo 'Skipping making predictions as this exists';
+else
+    python -m allennlp.run predict \
+        model.tar.gz \
+        data/model-specific/$MODEL/oracle/$1/$2 \
+        --output-file data/predictions/$MODEL/oracle/$1/$2 \
+        --predictor fever-oracle \
+        --include-package fever.reader \
+        --cuda-device -1 \
+        --silent
+fi;
