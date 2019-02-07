c649d64ca84edd038c267e02cde64f3f1b510cd8
James Thorne <james@jthorne.co.uk>
2019-02-07 11:36:39 +0000

    copy all experiment files into one directory:

--- src/experiment/aggregate_all.py
+++ src/experiment/aggregate_all.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 from pathlib import Path
 import re
+import os
+from shutil import copyfile
 parser = ArgumentParser()
 
 parser.add_argument("--family",type=str)
@@ -17,10 +19,15 @@ for path in pathlist:
     path_in_str = str(path)
     experiments.append(path_in_str.replace(args.actual_dir,""))
 
+os.makedirs(args.predicted_labels_dir+"/oracle/all")
+os.makedirs(args.predicted_labels_dir+"/full/all")
+
+os.makedirs(args.predicted_evidence_dir +"/oracle/all")
+os.makedirs(args.predicted_evidence_dir +"/full/all")
 
 for experiment_path in experiments:
     matches = re.match(r'/([a-z-]+)/([a-z-]+).(.+).jsonl',experiment_path)
-    experiment = matches.group(3)
+    experiment = str(matches.group(3))
 
     print(experiment)
 
@@ -38,3 +45,29 @@ for experiment_path in experiments:
     unchanged_new_actual_file = args.actual_dir + "/all/unchanged." +family+"."+ experiment + ".jsonl"
 
 
+    unchanged_oracle_old_predicted_file = args.predicted_labels_dir + "/oracle/" + family + "/unchanged." + experiment + ".jsonl"
+    unchanged_oracle_old_evidence_file = args.predicted_evidence_dir + "/oracle/" + family + "/unchanged." + experiment + ".jsonl"
+    unchanged_old_actual_file = args.actual_dir + "/" + family + "/unchanged." + experiment + ".jsonl"
+
+    changed_oracle_old_predicted_file = args.predicted_labels_dir + "/oracle/" + family + "/changed." + experiment + ".jsonl"
+    changed_oracle_old_evidence_file = args.predicted_evidence_dir + "/oracle/" + family + "/changed." + experiment + ".jsonl"
+    changed_old_actual_file = args.actual_dir + "/" + family + "/changed." + experiment + ".jsonl"
+
+    unchanged_full_old_predicted_file = args.predicted_labels_dir + "/full/" + family + "/unchanged." + experiment + ".jsonl"
+    unchanged_full_old_evidence_file = args.predicted_evidence_dir + "/full/" + family + "/unchanged." + experiment + ".jsonl"
+    changed_full_old_predicted_file = args.predicted_labels_dir + "/full/" + family + "/changed." + experiment + ".jsonl"
+    changed_full_old_evidence_file = args.predicted_evidence_dir + "/full/" + family + "/changed." + experiment + ".jsonl"
+
+
+    copyfile(unchanged_old_actual_file, unchanged_new_actual_file)
+
+    copyfile(unchanged_oracle_old_predicted_file, unchanged_oracle_new_predicted_file)
+    copyfile(unchanged_oracle_old_evidence_file, unchanged_oracle_new_evidence_file)
+    copyfile(unchanged_full_old_predicted_file, unchanged_full_new_predicted_file)
+    copyfile(unchanged_full_old_evidence_file, unchanged_full_new_evidence_file)
+
+    copyfile(changed_old_actual_file, changed_new_actual_file)
+    copyfile(changed_oracle_old_predicted_file, changed_oracle_new_predicted_file)
+    copyfile(changed_oracle_old_evidence_file, changed_oracle_new_evidence_file)
+    copyfile(changed_full_old_predicted_file, changed_full_new_predicted_file)
+    copyfile(changed_full_old_evidence_file, changed_full_new_evidence_file)
