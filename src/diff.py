7db9d6955719e35e6e737339263b8ef20e4cca0e
James Thorne <james@jthorne.co.uk>
2019-03-09 09:37:13 +0000

    use ordered set in original diff

--- src/diff.py
+++ src/diff.py
@@ -1,6 +1,65 @@
+import collections
 import json
-
+from collections import OrderedDict as dict
 from tqdm import tqdm
+
+class OrderedSet(collections.MutableSet):
+
+    def __init__(self, iterable=None):
+        self.end = end = []
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[1]
+            curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:
+            key, prev, next = self.map.pop(key)
+            prev[2] = next
+            next[1] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[2]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[2]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[1]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[1]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = self.end[1][0] if last else self.end[2][0]
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -28,11 +87,8 @@ if __name__ == "__main__":
 
             new_claims[claim["claim"]] = (claim,idx)
 
-    print(len(new_claims.keys()))
-    print(len(old_claims.keys()))
-    print(len(set(new_claims.keys()).intersection(old_claims.keys())))
-    diff = set(new_claims.keys()).difference(set(old_claims.keys()))
-    diff_minus = set(new_claims.keys()).intersection(old_claims.keys())
+    diff = OrderedSet(new_claims.keys()) - old_claims.keys()
+    diff_minus = OrderedSet(new_claims.keys()) & old_claims.keys()
 
     with open(args.diff_new, "w+") as diff_file:
         for claim in diff:
