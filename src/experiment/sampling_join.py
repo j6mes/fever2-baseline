e46e4425035923eb10c2f8746fd18cfd35cab785
James Thorne <james@jthorne.co.uk>
2019-06-10 10:41:49 +0100

    join sampled together

---
+++ src/experiment/sampling_join.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+import numpy as np
+import json
+import argparse
+
+np.random.seed(123)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--in-files",type=str)
+parser.add_argument("--out-file",type=str)
+
+args = parser.parse_args()
+
+claims = []
+
+for file in args.in_files.split(","):
+    with open(file) as f:
+        for line in f:
+            claims.append(json.loads(line))
+
+np.random.shuffle(claims)
+
+with open(args.test_file, "w+") as outfile:
+    for id, line in enumerate(claims):
+        line['id'] = id + 500000
+        line["attack"] = "redacted"
+        outfile.write(json.dumps(line) + "\n")
